ef54ab8718f201d19c0dd13c78a17ef47dc20b8a
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-11-19 20:55:32 -0500

    Added documentation for GET /hangars and GET /hangars/:hangarId

--- constants/hangarConstants.js
+++ constants/hangarConstants.js
@@ -8,7 +8,7 @@ const HANGAR_NAME_REGEX = /^[A-Za-z0-9 ./,-]+$/;
 
 const HANGAR_STATE_REGEX = /^[A-Za-z ]+$/;
 
-const HANGAR_CITY_REGEX = /^[A-Za-z .]+$/;
+const HANGAR_CITY_REGEX = /^[A-Za-z .,]+$/;
 
 module.exports = {
     MIN_CAPACITY,
